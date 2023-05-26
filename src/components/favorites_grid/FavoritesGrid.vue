8f0ba49155a23f619a3e2192e485da82bc8c39de
Tomasz Wrutniak <twrutniak@gmail.com>
2023-05-27 01:48:10 +0200

    feat: small refinements

--- src/components/favorites_grid/FavoritesGrid.vue
+++ src/components/favorites_grid/FavoritesGrid.vue
@@ -1,13 +1,18 @@
 <template>
   <section class="favorites-grid">
-    <FavoritesTile @favorite-tile-selected="onFavoriteTileSelected" v-for="feature in featureArray" :key="feature" :selected-feature="feature"></FavoritesTile>
+    <FavoritesTile @favorite-tile-selected="onFavoriteTileSelected" 
+                    v-for="feature in featureArray" :key="feature" 
+                    :selected-feature="feature"
+                    @v-on:click.alt="deleteFavorite(feature)">
+    </FavoritesTile>
   </section>
 </template>
 
 
 <script>
-import { getWeatherFavorites } from '@/helper_functions';
+import { getWeatherFavorites, removeWeatherFavorite } from '@/helper_functions';
 import FavoritesTile from './FavoritesTile.vue';
+import { Notify } from 'quasar'
 
 export default {
   name: 'FavoritesGrid',
@@ -33,7 +38,13 @@ export default {
       this.featureArray = getWeatherFavorites();
     },
     deleteFavorite: function(element) {
-
+      removeWeatherFavorite(element);
+      this.reloadFavorites();
+      Notify.create({
+          message: 'Success!',
+          caption: `Removed ${element} from favorites.`,
+          color: 'positive'
+      })
     },
   },
 
